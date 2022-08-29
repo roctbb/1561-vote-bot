94b1362f245d8e5ac1c441526c9d6d8bcb7717c6
roctbb <roctbb@gmail.com>
2022-08-29 15:41:04 +0300

    Initial commit

---
+++ 1561bot.py
@@ -0,0 +1,158 @@
+from telebot import TeleBot, types
+from config import token, default_balance, projects_count
+import json
+
+bot = TeleBot(token=token)
+
+state = {}
+
+try:
+    with open('state.json') as f:
+        state = json.load(f)
+except:
+    state = {
+        "projects": {
+            str(i): {
+                "count": 0,
+                "reviews": [],
+                "users": []
+            } for i in range(1, projects_count + 1)
+        },
+        "users": {}
+    }
+
+    print(state)
+
+
+def safe_send(to, m, keyboard=None):
+    try:
+        bot.send_message(to, m, reply_markup=keyboard)
+    except Exception as e:
+        print("Message error:", e)
+
+
+def save():
+    try:
+        with open('state.json', 'w') as f:
+            print(state)
+            f.write(json.dumps(state))
+    except Exception as e:
+        print("save error", e)
+
+
+def init_user(message):
+    if str(message.chat.id) not in state["users"]:
+        state["users"][str(message.from_user.id)] = {
+            "balance": default_balance,
+            "user_id": message.from_user.id,
+            "state": "project"
+        }
+
+        try:
+            state["users"][str(message.from_user.id)]["username"] = message.from_user.username
+        except:
+            pass
+
+        try:
+            state["users"][str(message.from_user.id)]["first_name"] = message.from_user.first_name
+            state["users"][str(message.from_user.id)]["last_name"] = message.from_user.last_name
+        except:
+            pass
+
+
+def make_projects_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    for i in range(1, projects_count + 1):
+        button = types.KeyboardButton(text="🗒 " + str(i))
+        keyboard.add(button)
+
+    return keyboard
+
+
+def make_money_keyboard(money_count):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    keyboard.add("Назад")
+    for i in range(1, money_count + 1):
+        button = types.KeyboardButton(text="💸 " + str(i))
+        keyboard.add(button)
+
+    return keyboard
+
+
+def make_voting(message):
+    if state["users"][str(message.from_user.id)]["balance"] > 0:
+        state["users"][str(message.from_user.id)]["state"] = "project"
+        safe_send(message.chat.id, "Выбери проект, закоторый хочешь проголосовать 👀.", make_projects_keyboard())
+    else:
+        state["users"][str(message.from_user.id)]["state"] = "finished"
+        safe_send(message.chat.id, "Спасибо за участие в голосовании! Ты вложил все свои монеты! 🙌")
+
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message):
+    init_user(message)
+
+    safe_send(message.chat.id, f"""Привет! Этот бот поможет тебе проинвестировать в один из социальных проектов, предложенных 10ми классами нашей школы 🤓. 
+    
+Сейчас у тебя 💸 {state["users"][str(message.from_user.id)]["balance"]} монет. Ты можешь выбрать любимый проект и вложить в него сразу все монеты или распределить свой капитал между несколькими.
+                               
+Проекты с наибольшим финансироваем получат поддержку от администрации ✊.""")
+
+    make_voting(message)
+    save()
+
+
+@bot.message_handler(content_types=['text'])
+def process_message(message):
+    init_user(message)
+
+    answer = str(message.text)
+    money_count = state["users"][str(message.from_user.id)]["balance"]
+
+
+    if state["users"][str(message.from_user.id)]["state"] == "project":
+        answer = answer.lstrip("🗒 ")
+        if answer.isnumeric() and int(answer) in range(1, projects_count + 1):
+            if money_count > 0:
+                state["users"][str(message.from_user.id)]["state"] = "money"
+                state["users"][str(message.from_user.id)]["chosen"] = answer
+                safe_send(message.chat.id, f'Сейчас у тебя 💸 {money_count} монет. Сколько хочешь проинвестировать в проект {answer}?', make_money_keyboard(money_count))
+            else:
+                safe_send(message.chat.id, "Монеты закончились.")
+        else:
+            safe_send(message.chat.id, "Выбери номер проекта...", keyboard=make_projects_keyboard())
+
+    elif state["users"][str(message.from_user.id)]["state"] == "money":
+        answer = answer.lstrip("💸 ")
+        if answer.isnumeric() and int(answer) in range(1, money_count + 1):
+            state["users"][str(message.from_user.id)]["state"] = "review"
+
+            chosen_project = state["users"][str(message.from_user.id)]["chosen"]
+            state["projects"][chosen_project]["count"] += int(answer)
+            state["projects"][chosen_project]["users"].append((str(message.from_user.id), int(answer)))
+
+            state["users"][str(message.from_user.id)]["balance"] -= int(answer)
+
+            safe_send(message.chat.id, "Расскажи, чем тебе понравилась эта идея? Что хотелось бы передать авторам? Ответ напиши одним сообщением... 🖌", types.ReplyKeyboardRemove())
+        else:
+            make_voting(message)
+
+    elif state["users"][str(message.from_user.id)]["state"] == "review":
+
+        chosen_project = state["users"][str(message.from_user.id)]["chosen"]
+        state["projects"][chosen_project]["reviews"].append((str(message.from_user.id), answer))
+
+        safe_send(message.chat.id, "Большое спасибо, твой голос сохранен! 👍")
+        make_voting(message)
+    else:
+        make_voting(message)
+
+    save()
+
+try:
+    bot.polling(none_stop=True)
+except Exception as e:
+    print("General exception:", e)
